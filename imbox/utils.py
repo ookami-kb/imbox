d0651da911ad4842e89f8bec5aec84f5b62caa6c
ookami.kb <ookami.kb@gmail.com>
2015-08-23 03:52:32 +0500

    updated util to py3 only

--- imbox/utils.py
+++ imbox/utils.py
@@ -3,6 +3,7 @@ def str_encode(value='', encoding=None, errors='strict'):
 
 
 def str_decode(value, encoding=None, errors='strict'):
+    print(value, encoding, errors)
     try:
         return bytes(value, encoding, errors).decode('utf-8')
     except:
