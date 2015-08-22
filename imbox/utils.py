df6df88b9b724efce9002546fd3922363c671aab
ookami.kb <ookami.kb@gmail.com>
2015-08-23 03:48:39 +0500

    updated util to py3 only

--- imbox/utils.py
+++ imbox/utils.py
@@ -1,18 +1,9 @@
-from __future__ import unicode_literals
-from six import PY3
+def str_encode(value='', encoding=None, errors='strict'):
+    return str(value, encoding, errors)
 
-if PY3:
-    def str_encode(value='', encoding=None, errors='strict'):
-        return str(value, encoding, errors)
 
-    def str_decode(value, encoding=None, errors='strict'):
-        try:
-            return bytes(value, encoding, errors).decode('utf-8')
-        except:
-            return value.decode(encoding)
-else:
-    def str_encode(string='', encoding=None, errors='strict'):
-        return unicode(string, encoding, errors)
-
-    def str_decode(value='', encoding=None, errors='strict'):
-        return value.decode(encoding, errors)
+def str_decode(value, encoding=None, errors='strict'):
+    try:
+        return bytes(value, encoding, errors).decode('utf-8')
+    except:
+        return value.decode(encoding)
